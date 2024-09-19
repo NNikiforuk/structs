a071054968a1b9b1620c621c9cbe1a47571be5d2
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-09-19 18:09:19 +0200

    Initial Commit

---
+++ structs/structsApp.swift
@@ -0,0 +1,17 @@
+//
+//  structsApp.swift
+//  structs
+//
+//  Created by Natalia Nikiforuk on 19/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct structsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
